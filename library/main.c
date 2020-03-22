46681ccded0e41e349c149ff12db4c14a1315837
DShimoda <dshimoda82@gmail.com>
2020-03-22 06:23:17 +0100

    Initial Commit

---
+++ library/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  library
+//
+//  Created by Shimoda on 22/03/2020.
+//  Copyright © 2020 Shimoda. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
